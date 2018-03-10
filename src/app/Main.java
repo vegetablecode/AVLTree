e05b46bac2059188d7f3f1dd234527af5e05f35a
Kamil <kamil4525@gmail.com>
2018-03-10 14:04:12 +0100

    added TreePrinter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,12 @@
 package app;
 
 import tree.AVLTree;
+import tree.Node;
+import view.TreePrinter;
 
 public class Main {
 
-	public static void main(String[] args) {
+	private static Node test1() {
 		AVLTree tree = new AVLTree();
 		
 		tree.root = tree.insert(tree.root, 10);
@@ -13,12 +15,29 @@ public class Main {
 		tree.root = tree.insert(tree.root, 40);
 		tree.root = tree.insert(tree.root, 50);
 		tree.root = tree.insert(tree.root, 25);
-		
-		tree.preOrder(tree.root);
+
+        return tree.root;
+    }
+
+    public static void main(String[] args) {
+
+    		AVLTree tree = new AVLTree();
+    		tree.root = tree.insert(tree.root, 10);
+    		tree.root = tree.insert(tree.root, 20);
+    		tree.root = tree.insert(tree.root, 30);
+    		tree.root = tree.insert(tree.root, 40);
+    		tree.root = tree.insert(tree.root, 50);
+    		tree.root = tree.insert(tree.root, 25);
+    		
+    		tree.preOrder(tree.root);
 		System.out.println(" ");
 		tree.inOrder(tree.root);
 		System.out.println(" ");
 		tree.postOrder(tree.root);
-	}
+		System.out.println(" ");
+		
+        TreePrinter.printNode(test1());
 
+    }
+    
 }
---
+++ src/view/TreePrinter.java
@@ -0,0 +1,96 @@
+// This is a tree-printer created by michal.kreuzman and modified by me.
+// ORIGINAL PRINTER HERE: https://stackoverflow.com/questions/4965335/how-to-print-binary-tree-diagram
+
+package view;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import tree.Node;
+
+public class TreePrinter {
+
+	 public static <T extends Comparable<?>> void printNode(Node root) {
+	        int maxLevel = TreePrinter.maxLevel(root);
+
+	        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+	    }
+
+	    private static <T extends Comparable<?>> void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
+	        if (nodes.isEmpty() || TreePrinter.isAllElementsNull(nodes))
+	            return;
+
+	        int floor = maxLevel - level;
+	        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+	        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+	        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+	        TreePrinter.printWhitespaces(firstSpaces);
+
+	        List<Node> newNodes = new ArrayList<Node>();
+	        for (Node node : nodes) {
+	            if (node != null) {
+	                System.out.print(node.getData());
+	                newNodes.add(node.getLeftChild());
+	                newNodes.add(node.getRightChild());
+	            } else {
+	                newNodes.add(null);
+	                newNodes.add(null);
+	                System.out.print(" ");
+	            }
+
+	            TreePrinter.printWhitespaces(betweenSpaces);
+	        }
+	        System.out.println("");
+
+	        for (int i = 1; i <= endgeLines; i++) {
+	            for (int j = 0; j < nodes.size(); j++) {
+	                TreePrinter.printWhitespaces(firstSpaces - i);
+	                if (nodes.get(j) == null) {
+	                    TreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+	                    continue;
+	                }
+
+	                if (nodes.get(j).getLeftChild() != null)
+	                    System.out.print("/");
+	                else
+	                    TreePrinter.printWhitespaces(1);
+
+	                TreePrinter.printWhitespaces(i + i - 1);
+
+	                if (nodes.get(j).getRightChild() != null)
+	                    System.out.print("\\");
+	                else
+	                    TreePrinter.printWhitespaces(1);
+
+	                TreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+	            }
+
+	            System.out.println("");
+	        }
+
+	        printNodeInternal(newNodes, level + 1, maxLevel);
+	    }
+
+	    private static void printWhitespaces(int count) {
+	        for (int i = 0; i < count; i++)
+	            System.out.print(" ");
+	    }
+
+	    private static <T extends Comparable<?>> int maxLevel(Node node) {
+	        if (node == null)
+	            return 0;
+
+	        return Math.max(TreePrinter.maxLevel(node.getLeftChild()), TreePrinter.maxLevel(node.getRightChild())) + 1;
+	    }
+
+	    private static <T> boolean isAllElementsNull(List<T> list) {
+	        for (Object object : list) {
+	            if (object != null)
+	                return false;
+	        }
+
+	        return true;
+	    }
+}
